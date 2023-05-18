b1b2c8ad8de82d11f1d7832b4f1fcbd0b463899a
ddkomov <ddkomov@sberbank.ru>
2023-05-18 18:27:21 +0300

    I. Введение в Java и JUnit (Тест 2 Попытка 1)

--- MainClassTest.java
+++ MainClassTest.java
@@ -1,3 +1,4 @@
+import jdk.jfr.Name;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,5 +11,10 @@ public class MainClassTest extends MainClass {
         } else {
            Assert.fail("Method getLocalNumber did not return 14");
         }
+    }
+    @Test
+    @Name("Проверка, что getClassNumber возвращает число больше 45")
+    public void testGetClassNumber() {
+        Assert.assertTrue("Method getClassNumber did not return number, that bigger than 45",getClassNumber() > 45);
     }
 }
